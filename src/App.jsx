0c498c70250017ef5c314f861c9b1ae780b29667
Kenneth Ward Jr <kenwardjr8592@gmail.com>
2024-12-01 22:08:30 -0800

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-
-
-const todoList = [
-  { id: 1, title: "Complete assignment" },
-  { id: 2, title: "Buy groceries" },
-  { id: 3, title: "Schedule a meeting" },
-];
+import TodoList from './TodoList'; git
+import AddTodoForm from './AddTodoForm'; 
 
 function App() {
   return (
     <div>
-      
       <h1>Todo List</h1>
-
-     
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <AddTodoForm /> 
+      <TodoList /> 
     </div>
   );
 }
 
 export default App;
+
+
+
